3809d5fb6be27c7f51b55fa2ae6d120da2dfa855
anshuman7negi <anshumannegi777@gmail.com>
2023-10-27 14:02:03 +0530

    added test cases for model

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+  it "increments the post's comments_counter after create" do
+    user = User.create(name: 'Roy Batty', posts_counter: 5)
+    post = Post.create(title: 'Sample Title', author: user, text: 'This is my first post', comments_counter: 0, likes_counter: 0)
+    Comment.create(user:, post:)
+    expect(post.reload.comments_counter).to eq(1)
+  end
+
+  it "decrements the post's comments_counter after destroy" do
+    user = User.create(name: 'Roy Batty', posts_counter: 5)
+    post = Post.create(title: 'Sample Title', author: user, text: 'This is my first post', comments_counter: 0, likes_counter: 0)
+    comment = Comment.create(user:, post:)
+    comment.destroy
+    expect(post.reload.comments_counter).to eq(0)
+  end
+end
---
+++ spec/models/post_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  let(:first_user) { User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.', posts_counter: 5) }
+  subject { described_class.new(author: first_user, title: 'Hello world', text: 'This is my first post', comments_counter: 0, likes_counter: 0) }
+
+  before { subject.save }
+
+  # Validation tests
+  describe 'Validations' do
+    it 'is valid with valid attributes' do
+      expect(subject).to be_valid
+    end
+
+    it 'is not valid without a title' do
+      subject.title = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid without a text' do
+      subject.text = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'validates the presence of comments_counter' do
+      subject.comments_counter = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'validates the presence of likes_counter' do
+      subject.likes_counter = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'validates that comments counter is a positive integer' do
+      subject.comments_counter = -1
+      expect(subject).to_not be_valid
+    end
+
+    it 'validates that likes counter is a positive integer' do
+      subject.likes_counter = -1
+      expect(subject).to_not be_valid
+    end
+  end
+
+  # Method tests
+  describe 'Methods' do
+    it 'Returns empty if the post has no comments' do
+      expect(subject.recent_5_comments).to eq([])
+    end
+  end
+end
