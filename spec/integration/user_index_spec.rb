6a4c26679c0b7102b07dfac1f2ed68622d15e0af
anshuman7negi <anshumannegi777@gmail.com>
2023-11-07 17:43:48 +0530

    solved linter errors

--- spec/integration/user_index_spec.rb
+++ spec/integration/user_index_spec.rb
@@ -3,12 +3,13 @@ RSpec.describe 'Users Page', type: :system do
   before do
     User.destroy_all
     Post.destroy_all
-    
+
     @first_user = User.create(name: 'Tom', bio: 'Teacher from Mexico.', posts_counter: 0, photo: 'https://example.com/user1.jpg')
     @second_user = User.create(name: 'som', bio: 'Teacher from Mexico.', posts_counter: 4, photo: 'https://example.com/user1.jpg')
     @third_user = User.create(name: 'pom', bio: 'Teacher from Mexico.', posts_counter: 5, photo: 'https://example.com/user1.jpg')
-    
-    @post1 = Post.create(author: @first_user, title: 'Post1', text: 'This is the first post.', comments_counter: 0, likes_counter: 0)
+
+    @post1 = Post.create(author: @first_user, title: 'Post1', text: 'This is the first post.', comments_counter: 0,
+                         likes_counter: 0)
   end
   scenario 'display users when there are users' do
     visit root_path
