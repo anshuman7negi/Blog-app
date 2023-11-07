d8d7a65809cbe446ad570be0a9fcd53d06b41df5
anshuman7negi <anshumannegi777@gmail.com>
2023-11-07 13:49:30 +0530

    added test cases for user_show

---
+++ spec/integration/posts_index_spec.rb
@@ -0,0 +1,65 @@
+require 'rails_helper'
+
+RSpec.describe 'User Posts Page', type: :system do
+  before do
+    User.destroy_all
+    Post.destroy_all
+
+    @user = User.create(name: 'Tom', bio: 'Teacher from Mexico.', posts_counter: 0, photo: 'https://example.com/user1.jpg')
+    @post1 = Post.create(author: @user, title: 'Post1', text: 'This is the first post.', comments_counter: 0, likes_counter: 0)
+  end
+
+  scenario 'displays user profile information and no posts' do
+    visit user_posts_path(@user)
+    
+    expect(page).to have_selector("img[src*='https://example.com/user1.jpg']")
+    expect(page).to have_content('Tom')
+    expect(page).to have_content('Number of posts: 1')
+    expect(page).to have_button('Create new post')
+    expect(page).to have_content('There are no comments for this post.')
+    expect(page).to have_content("Comments: #{@post1.comments_counter}")
+    expect(page).to have_content("Likes: #{@post1.likes_counter}")
+  end
+
+  scenario 'displays user posts with comments and likes' do
+    post = Post.create(author: @user, title: 'Post2', text: 'This is the second post.', comments_counter: 2, likes_counter: 5)
+
+    comment1 = Comment.create(post: post, user: @user, text: 'Great post!')
+    comment2 = Comment.create(post: post, user: @user, text: 'I love it!')
+
+    visit user_posts_path(@user)
+
+    expect(page).to have_content('This is the first post.')
+    expect(page).to have_content("Comments: #{post.comments_counter}")
+    expect(page).to have_content("Likes: #{post.likes_counter}")
+
+    expect(page).to have_content('Tom: Great post!')
+    expect(page).to have_content('Tom: I love it!')
+  end
+
+  scenario 'pagination button redirects to correct post' do
+    visit user_posts_path(@user)
+
+    click_button('Pagination')
+    sleep(2)
+
+    expect(current_path).to eq(user_post_path(@user, @post1))
+  end
+
+  scenario 'click the user posts' do
+    last_post = Post.create(author: @user, title: 'last post', text: 'This is the last post.', comments_counter: 2, likes_counter: 5)
+    visit user_path(@user)
+    click_link 'Post #1'
+    sleep(2)
+    expect(current_path).to eq(user_post_path(@user,last_post))
+  end
+
+  scenario 'click "Create new post" button' do
+    visit user_posts_path(@user)
+
+    click_button('Create new post')
+    sleep(2)
+
+    expect(current_path).to eq(new_user_post_path(@user))
+  end
+end
---
+++ spec/integration/user_show_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+RSpec.describe 'User Show Page', type: :system do
+  before do
+    User.destroy_all
+    Post.destroy_all
+    
+    @first_user = User.create(name: 'Tom', bio: 'Teacher from Mexico.', posts_counter: 0, photo: 'https://example.com/user1.jpg')
+    
+    @post1 = Post.create(author: @first_user, title: 'Post1', text: 'This is the first post.', comments_counter: 0, likes_counter: 0)
+    @post2 = Post.create(author: @first_user, title: 'Post2', text: 'This is the second post.', comments_counter: 0, likes_counter: 0)
+    @post3 = Post.create(author: @first_user, title: 'Post3', text: 'This is the third post.', comments_counter: 0, likes_counter: 0)
+    @post4 = Post.create(author: @first_user, title: 'Post4', text: 'This is the fourth post.', comments_counter: 0, likes_counter: 0)
+
+  end
+
+  scenario 'displays the user profile information' do
+    visit user_path(@first_user)
+    expect(page).to have_selector("img[src*='https://example.com/user1.jpg']")
+    expect(page).to have_content('Tom')
+    expect(page).to have_content('Number of posts: 4')
+    expect(page).to have_content('Teacher from Mexico')
+    
+
+    [@post4, @post3, @post2].each do |post|
+      expect(page).to have_content(post.text)
+    end
+
+    click_link 'See all posts'
+    sleep(2)
+    expect(current_path).to eq(user_posts_path(@first_user))
+  end
+
+  scenario 'click the user posts' do
+    visit user_path(@first_user)
+    click_link 'Post #1'
+    sleep(2)
+    expect(current_path).to eq(user_post_path(@first_user,@post4))
+  end
+
+  scenario 'handles no recent posts' do
+    @second_user = User.create(name: 'Alice', bio: 'Developer from USA.', posts_counter: 0, photo: 'https://example.com/user2.jpg')
+    visit user_path(@second_user)
+
+    expect(page).to have_content('There are no posts by this user.')
+    expect(page).not_to have_button('See all posts')
+  end
+end
