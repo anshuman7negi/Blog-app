56aeec69b253fbc393129280fc73382537193006
anshuman7negi <anshumannegi777@gmail.com>
2023-11-08 15:02:06 +0530

    added confirmation email

---
+++ db/migrate/20231108083814_add_devise_confirmable_to_users.rb
@@ -0,0 +1,16 @@
+class AddDeviseConfirmableToUsers < ActiveRecord::Migration[7.1]
+  def self.up
+    change_table :users do |t|
+      t.string   :confirmation_token
+      t.datetime :confirmed_at
+      t.datetime :confirmation_sent_at
+      t.string   :unconfirmed_email
+    end
+
+    add_index :users, :confirmation_token, unique: true
+  end
+
+  def self.down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
