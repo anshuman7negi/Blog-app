e01e7fc32bcd373e318c77af94d02e7366e7daea
anshuman7negi <anshumannegi777@gmail.com>
2023-11-09 12:34:41 +0530

    updated abilty class

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,14 +5,12 @@ class Ability
 
   def initialize(user)
     return unless user.present?
-
     can :read, :all
     can :create, Post
     can :destroy, Post, author_id: user.id
     can %i[create destroy], Comment, author_id: user.id
     
-    return unless user.admin?
-
+    return unless user.role == 'admin'
     can :destroy, Post
     can :destroy, Comment
     # Define abilities for the user here. For example:
